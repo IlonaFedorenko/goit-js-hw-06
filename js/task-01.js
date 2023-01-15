dd5b5e1569dc69d8990e60ac6e8df88874f96e30
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-01-15 18:56:49 +0100

    fix dz

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,5 @@
-// Number of categories: 3
-// Category: Animals
-// Elements: 4
-// Category: Products
-// Elements: 3
-// Category: Technologies
-// Elements: 5
-
 const categoryEl = document.querySelectorAll(".item");
 console.log('Number of categories:', categoryEl.length);
-
-// categoryList.forEach((elemant) => {
-//     console.log(`Category: ${elemant.querySelector('h2').textContent}`);
-//     console.log(`Elements: ${elemant.querySelectorAll('h2+ul>li').length}`);
-// });
 
 const categoryList = categoryEl.forEach((elemant) => {
     console.log('Category:', elemant.firstElementChild.textContent);
--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,3 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input),
-// подставляет его текущее значение в span#name - output.Если инпут пустой,
-// в спане должна отображаться строка "Anonymous".
-
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
-
-// При вводе в инпут меняется строка снизу с Anonymous на ту что ввели в инпут
- // соответсвенно если ничего не введено то так и остается строка Hello, Anonymous!
 const idInput = document.querySelector('#name-input');
 console.log(idInput);
 const outputEl = document.querySelector('#name-output');
@@ -19,6 +10,3 @@ idInput.addEventListener("input", (event) => {
         outputEl.textContent = event.currentTarget.value;
     }
 });
-
-// idInput.addEventListener("input", (event) => {
-//     outputEl.textContent = event.currentTarget.value; });
