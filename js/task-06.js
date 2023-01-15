2e68e5b1309c85fca1706a30ffcb4a7a31ad3543
IlonaFedorenko <86719203+IlonaFedorenko@users.noreply.github.com>
2023-01-15 19:23:44 +0100

    fix dz6

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,14 @@ const length = Number(idInput.dataset.length);
 console.log(length);
 
 idInput.addEventListener("blur", (event) => {
-     idInput.classList.add('invalid');
+    //  idInput.classList.add('invalid');
     if (event.currentTarget.value.length === length) {
-        idInput.classList.replace('invalid', 'valid');
-    } 
+         idInput.classList.add('valid');
+         idInput.classList.remove('invalid');
+    } else {
+         idInput.classList.add('invalid');
+         idInput.classList.remove('valid');
+        }
 });
 
 
